4f873f4b572e7ff24140039ac626aa541dc745b2
jps7777777 <33079475+jps7777777@users.noreply.github.com>
2017-12-03 15:55:10 +0800

    php string test
    string test

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/24
+ * Time: 10:47
+ */
+
+require 'libs/config/SmartyConfig.php';
+require 'string.php';
+
+//if(!empty($_SESSION)){
+//    var_dump($_SESSION);
+//}
+//var_dump($_COOKIE);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/3
+ * Time: 15:29
+ */
+
+
+
+$smarty->assign('result','');
+$smarty->display('string.html');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
